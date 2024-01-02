cfce6ff9c7ed86c7bd439c3c6c510c0ba60a5e7b
andriansyah <anandri394@gmail.com>
2024-01-02 19:45:44 +0700

    first commit

---
+++ Luxspace/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-600">Hello World, Iam Using React dan Vite in Website</h1>
+    </>
+  );
+}
+
+export default App;
