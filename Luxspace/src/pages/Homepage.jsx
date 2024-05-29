18556129d1dd3ddd934fd35d046d31615c40a5b9
andriiannsyah <anandri394@gmail.com>
2024-05-29 11:28:26 +0700

    add : useScroll dan useModal

--- Luxspace/src/pages/Homepage.jsx
+++ Luxspace/src/pages/Homepage.jsx
@@ -1,12 +1,16 @@
 import Header from "../parts/Header";
-import Hero from "../parts/Hero";
+import Hero from "../parts/Homepage/Hero";
 import BrowseRoom from "../parts/Homepage/BrowseRoom";
 import JustArrived from "../parts/Homepage/JustArrived";
 import Clients from "../parts/Clients";
 import Sitemap from "../parts/Sitemap";
 import Footer from "../parts/footer";
+import useScrollAnchor from "../helpers/hooks/useScrollAnchor";
+import useModalDOM from "../helpers/hooks/useModalDOM.JSX";
 
 export default function Homepage() {
+  useScrollAnchor();
+  useModalDOM();
   return (
     <>
       <Header theme="white" position="absolute" />
